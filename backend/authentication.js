c6715a76a93e709a27c4b5e9e4969841e714fdf2
sagzone <spkawad21@gmail.com>
2024-06-06 12:19:19 +0530

    getting users functionality added

--- backend/authentication.js
+++ backend/authentication.js
@@ -6,34 +6,68 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const UsersDB = [
+  { username: "sagar", pass: "123123" },
+  { username: "shyam", pass: "abudhabi" },
+  { username: "pangunath", pass: "shadab123" },
+];
+
+function userDetails(username, pass) {
+  let userDetails = { user: false, pass: false };
+
+  for (const user of UsersDB) {
+    if (username == user.username) {
+      userDetails.user = true;
+      if (pass == user.pass) {
+        userDetails.pass = true;
+      }
+    }
+  }
+  return userDetails;
+}
+
 app.post("/signin", function (req, res) {
   const body = req.body;
 
-  console.log(`received: ${JSON.stringify(body)}`);
-  //   console.log("request sent");
-  //   res.json({
-  //     username: username,
-  //     pass: pass,
-  //   });
+  const userd = userDetails(body.username, body.pass);
 
-  const token = jwt.sign(body.username, "secret");
-  res.json({ token: token });
+  if (userd.user) {
+    if (userd.pass) {
+      try {
+        const token = jwt.sign(body.username, "secret");
+        res.json({ token });
+      } catch (e) {
+        res.send(e);
+      }
+    } else {
+      res.json({ msg: "pass incorrect!" });
+    }
+  }
+
+  res.json({ msg: "user does not exists!" });
 });
 
 app.get("/users", function (req, res) {
   const authorization = req.headers["authorization"];
   console.log(authorization);
+  let decodedUser;
 
   try {
-    jwt.verify(authorization, "secret");
+    decodedUser = jwt.verify(authorization, "secret");
+    console.log(decodedUser);
   } catch (e) {
     res.send(`error: ${e}`);
   }
 
-  res.json([
-    { name: "sam", age: "18" },
-    { name: "shambhu", age: 16 },
-  ]);
+  const userArr = [];
+
+  for (const user of UsersDB) {
+    if (decodedUser != user.username) {
+      userArr.push(user);
+    }
+  }
+
+  res.json(userArr);
 });
 
 app.listen(3000, () => {
