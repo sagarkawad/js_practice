498e49cff3ca3e79c17e6e663728246267eabeca
sagzone <spkawad21@gmail.com>
2024-06-06 23:29:43 +0530

    multi-page functionality added with jugaad

--- frontend/react-app/src/App.jsx
+++ frontend/react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import SignIn from "./pages/SignIn/SignIn";
 import DisplayBoard from "./pages/DisplayBoard/DisplayBoard";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -11,15 +12,49 @@ function App() {
     setSignedInUser(localStorage.getItem("user"));
   }, []);
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <nav>
+            <Link to="/" className="mr-8">
+              Sign In
+            </Link>
+            <Link to="/users">Users</Link>
+          </nav>
+          <SignIn
+            setSignedInUser={setSignedInUser}
+            setOtherUsers={setOtherUsers}
+          />
+        </>
+      ),
+    },
+    {
+      path: "users",
+      element: (
+        <>
+          <nav>
+            <Link to="/" className="mr-8">
+              Sign In
+            </Link>
+            <Link to="/users">Users</Link>
+          </nav>
+          <DisplayBoard
+            signedInUser={signedInUser}
+            otherUsers={otherUsers}
+            setOtherUsers={setOtherUsers}
+          />
+        </>
+      ),
+    },
+  ]);
+
   return (
     <div className="p-4">
       <h1 className="text-red-500">React App</h1>
-      <SignIn setSignedInUser={setSignedInUser} setOtherUsers={setOtherUsers} />
-      <DisplayBoard
-        signedInUser={signedInUser}
-        otherUsers={otherUsers}
-        setOtherUsers={setOtherUsers}
-      />
+
+      <RouterProvider router={router} />
     </div>
   );
 }
